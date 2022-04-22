123ff7764b1f263f2e9d8fb78ba59b1c4ffff4c0
nanzong <iszong@yeah.net>
2022-04-22 21:33:07 +0800

    第一次提交 a=b=1

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * FileName: Test
+ * Author: nanzong
+ * Date: 2022/4/22
+ * Description:
+ * History:
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a = 1;
+        String b = "1";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
